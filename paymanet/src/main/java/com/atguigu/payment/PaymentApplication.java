2d75f2f79bbfffb9a86b30f442d634273300e7eb
moli <2487580445@qq.com>
2023-08-24 21:30:05 +0800

    zookeeper01

--- paymanet/src/main/java/com/atguigu/payment/PaymentApplication.java
+++ paymanet/src/main/java/com/atguigu/payment/PaymentApplication.java
@@ -4,12 +4,10 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 @MapperScan("com.atguigu.payment.mapper")
-@EnableEurekaClient
 @EnableDiscoveryClient
 public class PaymentApplication {
     public static void main(String[] args) {
--- paymanet/src/main/java/com/atguigu/payment/controller/PaymentController.java
+++ paymanet/src/main/java/com/atguigu/payment/controller/PaymentController.java
@@ -26,29 +26,12 @@ public class PaymentController {
     @Resource
     private DiscoveryClient discoveryClient;
 
-    @GetMapping("{id}")
-    public Result<Payment> get(@PathVariable("id") Integer id) {
-        Payment payment = paymentService.getById(id);
-        log.info("serverPort={}",serverPort);
-        return Result.success(payment);
-    }
-
-
-    @PostMapping
-    public Result<Void> add(@RequestBody Payment payment) {
-        boolean b = paymentService.save(payment);
-
-        System.out.println("11111");
-        log.info("插入结果：{}",b);
-        return b ? Result.success(): Result.error();
-    }
-
-    @GetMapping
-    public Result<Void> dis() {
+    @GetMapping("zk")
+    public Result<String> zk() {
         List<String> services = discoveryClient.getServices();
         services.forEach(log::info);
 
-        List<ServiceInstance> instances = discoveryClient.getInstances("CLOUD-PAYMENT");
+        List<ServiceInstance> instances = discoveryClient.getInstances("cloud-payment");
         instances.forEach(serviceInstance ->  {
             log.info("{}\t{}\t{}\t{}",serviceInstance.getInstanceId(),serviceInstance.getPort(),serviceInstance.getUri(),serviceInstance.getMetadata());
         });
